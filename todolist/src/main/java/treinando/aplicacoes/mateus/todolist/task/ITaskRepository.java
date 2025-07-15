c9fcf7fa3cb551cd6409fa6e23c219edbd72edd7
Mateus-denv <mateussant0sc0osta@outlook.com>
2025-07-15 19:45:13 -0300

    Create ITaskRepository.java

---
+++ todolist/src/main/java/treinando/aplicacoes/mateus/todolist/task/ITaskRepository.java
@@ -0,0 +1,10 @@
+package treinando.aplicacoes.mateus.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
+    
+}
