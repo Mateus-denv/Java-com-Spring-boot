af04796c261250bdbb2cd6b9ae736589c9278fde
Mateus-denv <mateussant0sc0osta@outlook.com>
2025-07-15 08:43:48 -0300

    Create TodolistApplication.java

---
+++ todolist/src/main/java/treinando/aplicacoes/mateus/todolist/TodolistApplication.java
@@ -0,0 +1,13 @@
+package treinando.aplicacoes.mateus.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class TodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplication.class, args);
+	}
+
+}
